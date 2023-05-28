8bbb1ff13af489fc0f2fe7b17332e299bc7ec3ea
Ulfar Johnson <ulfarjohnson@example.com>
2023-05-28 17:17:24 +0000

    baetti lab skrar fyrir week 1

---
+++ week1/src/Card.js
@@ -0,0 +1,15 @@
+
+function Card(props) {
+    return (
+        <div className>
+            <h2>
+                {props.h2}
+            </h2>
+            <h3>
+                {props.h3}
+            </h3>
+        </div>
+    );
+};
+
+export default Card;
